c33aa440d65d522066f74b1b775bd1447d87fba3
MrGhost115 <44943009+MrGhost115@users.noreply.github.com>
2018-12-27 14:31:06 +0100

    Registration.php
    - Added registration.php
    - Fixed bug(OnlineStatus)
    - Added password hashing

--- send.php
+++ send.php
@@ -1,20 +1,20 @@
 <?php
 	session_start();
-	if(isset($_GET['message']) && $_SESSION['logged']==1){
+	if(isset($_GET['message']) && $_SESSION['logged']==1 && $_GET['message']!=""){
 		require_once 'db_connect.php';
 		$conn = new mysqli($dbServername, $dbUsername, $dbPassword, $dbName);
 		
 		if ($conn->connect_error) {
-			die("0");
+			die("2");
 		}
 		
 		$message=htmlentities($_GET['message']);
 		
 		$sql = 'INSERT INTO messages (ID_Sender, ID_Conversation, Text) 
-		VALUES ('.$_SESSION['ID_USER'].', '.$_SESSION['ID_CONV'].', '.$message.') ';
+		VALUES ('.$_SESSION['ID_USER'].', '.$_SESSION['ID_CONV'].', "'.$message.'") ';
 		$result = $conn->query($sql);
 		$conn->close();
 		
-		if($result==1) echo 1; else echo 0;  //Recive 1 when everything is ok, 0 when something went wrong
-	}else echo 0;
+		if($result==1) echo 1; else echo 3;  //Recive 1 when everything is ok, 0 when something went wrong
+	}else echo 5;
 ?>
